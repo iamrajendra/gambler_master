8c948d23a6ab3c1eee15f5c2a730efb7f1f76bad
rajendra_walkover <rajendra@walkover.in>
2019-03-10 19:33:18 +0530

    edit sample and add video

--- app/src/main/java/com/gamblerindore/MainActivity.java
+++ app/src/main/java/com/gamblerindore/MainActivity.java
@@ -13,12 +13,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.buttonPanel).setOnClickListener(this);
+        findViewById(R.id.error_toast).setOnClickListener(this);
+        findViewById(R.id.info_toast).setOnClickListener(this);
+        findViewById(R.id.warning_toast).setOnClickListener(this);
+        findViewById(R.id.error_sb).setOnClickListener(this);
+        findViewById(R.id.info_sb).setOnClickListener(this);
+        findViewById(R.id.warning_sb).setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        new Message(this).success("Working").show();
+        switch (v.getId()){
+            case R.id.error_toast:
+                new Message(this).error("this is a error message").show();
+                break;
+            case R.id.info_toast:
+                new Message(this).information("this is a information message").show();
+                break;
+            case R.id.warning_toast:
+                new Message(this).warning("this is a warning message").show();
+                break;
+
+            case R.id.error_sb:
+                new Snakebar(this).error("this is a error message").show();
+                break;
+            case R.id.info_sb:
+                new Snakebar(this).information("this is a information message").show();
+                break;
+            case R.id.warning_sb:
+                new Snakebar(this).warning("this is a warning message").show();
+                break;
+
+        }
+
     }
 }
